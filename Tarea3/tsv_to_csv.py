7d61b833bfc828698979d64a6761616c7367c50a
Nicolás Acevedo Villena <ni.acevedo.villena@gmail.com>
2022-05-27 21:36:04 -0400

    Update tsv_to_csv.py

--- Tarea3/tsv_to_csv.py
+++ Tarea3/tsv_to_csv.py
@@ -2,7 +2,7 @@
 # Change tabs to commas in a csv file "AFINN_full.csv"
 import pandas as pd
 from requests import head
-df = pd.read_csv('AFINN_full.csv', sep='\t', header=None)
-df.to_csv('AFINN_full.csv', sep=',', index=False)
+df = pd.read_csv('AFINN_full_old.csv', sep='\t', header=None)
+df.to_csv('AFINN_full.csv', sep=',', index=False, header=False)
 df.head()
 # %%
